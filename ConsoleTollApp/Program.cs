279ff832bda2ec8e7007042c556e1792bfb29cfb
Peter John Alvarado <redjoker011@gmail.com>
2022-03-15 21:35:28 +0800

    * .gitignore: Create Initial Classes
    * ConsoleTollApp.sln:
    * Program.cs:
    * TollCalculator.cs:
    * ExternalSystems.cs:
    * ConsoleTollApp.csproj:
    * ConsoleTollApp.csproj.user:
    * AssemblyInfo.cs:

---
+++ ConsoleTollApp/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+using CommercialRegistration;
+using ConsumerVehicleRegistration;
+using LiveryRegistration;
+using Calculators;
+
+var tollCalc = new TollCalculator();
+
+var car = new Car();
+var taxi = new Taxi();
+var bus = new Bus();
+var truck = new DeliveryTruck();
+
+Console.WriteLine($"The toll for a car is {tollCalc.CalculateToll(car)}");
+Console.WriteLine($"The toll for a taxi is {tollCalc.CalculateToll(taxi)}");
+Console.WriteLine($"The toll for a bus is {tollCalc.CalculateToll(bus)}");
+Console.WriteLine($"The toll for a truck is {tollCalc.CalculateToll(truck)}");
+
+try
+{
+    tollCalc.CalculateToll("this will fail");
+}
+catch (ArgumentException e)
+{
+    Console.WriteLine("Caught an argument exception when using the wrong type");
+}
+try
+{
+    tollCalc.CalculateToll(null!);
+}
+catch (ArgumentNullException e)
+{
+    Console.WriteLine("Caught an argument exception when using null");
+}
---
+++ ConsoleTollApp/TollCalculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using CommercialRegistration;
+using ConsumerVehicleRegistration;
+using LiveryRegistration;
+
+namespace Calculators
+{
+    public class TollCalculator
+    {
+        public decimal CalculateToll(object vehicle) => vehicle switch
+        {
+            Car c => 2.00m,
+            Taxi t => 3.50m,
+            Bus b => 5.00m,
+            DeliveryTruck t => 10.00m,
+            { } => throw new ArgumentException(message: "Not a known vehicle type", paramName: nameof(vehicle)),
+            null => throw new ArgumentNullException(nameof(vehicle))
+        };
+    }
+}
